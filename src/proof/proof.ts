00039e6dbfde87a345779453d208b8b98d3eddc4
kotagit75 <unity75lets5@gmail.com>
2026-03-11 17:06:42 +0900

    add features: compareTime and calcVDF

--- src/proof/proof.ts
+++ src/proof/proof.ts
@@ -1,24 +1,35 @@
-import z from "zod"
-import { keyToPk, median, pkToKey, skToKey } from "./util"
+import { createHash } from "crypto"
+import { keyToPk, pkToKey, skToKey } from "./util"
+import { computeVDF, createVDFProof, verifyVDF, type VDFProof } from "./vdf"
 
 export type Address = string
 export type Signature = number[]
 
 class Stamp{
-    constructor(public address: Address, public count: number, public sign: Signature){}
+    constructor(public address: Address, public count: number, public vdfResult: VDFProof, public sign: Signature){}
     toStringForSign(pk: string): string{
-        return stampToStringForSign(pk, this.address, this.count)
+        return stampToStringForSign(pk, this.address, this.count, this.vdfResult)
     }
 }
-const stampToStringForSign = (pk: string, address: Address, count: number): string => pk + address + String(count)
-const isValidStamp = (stamp: Stamp, pk: string): boolean => stamp.count>=0 && pkToKey(stamp.address).verify(stamp.toStringForSign(pk), Buffer.from(stamp.sign))
-
+const stampToStringForSign = (pk: string, address: Address, count: number, vdfResult: VDFProof): string => pk + address + String(count) + String(vdfResult)
+const calcStampHashForVDF = (address: Address, count: number): bigint => BigInt("0x"+createHash("sha256").update(address + String(count)).digest('hex'))
+const N = 9999999967n * 9999999973n
+const T = 100000
+const calcVDFResult = (address: Address, count: number): VDFProof => {
+    const hash = calcStampHashForVDF(address, count)
+    return createVDFProof(hash, computeVDF(hash, T, N), T, N)
+}
+const isValidStamp = (stamp: Stamp, pk: string): boolean => {
+    const isValidCount = stamp.count>=0
+    const isValidVDF = verifyVDF(calcStampHashForVDF(stamp.address, stamp.count), stamp.vdfResult, T, N)
+    const isValidSign = pkToKey(stamp.address).verify(stamp.toStringForSign(pk), Buffer.from(stamp.sign))
+    return isValidCount && isValidVDF && isValidSign
+}
 class Proof{
     constructor(public data: string, public stamps: Stamp[], public sk: string, public address: Address, public sign: Signature){}
     toStringForSign(){
         return proofToStringForSign(this.data, this.stamps, this.sk, this.address)
     }
-    public calcTime = (): number => median(this.stamps.map((stamp, _, __) => stamp.count ?? 0))
 }
 const proofToStringForSign = (data: string, stamps: Stamp[], sk: string, address: Address): string => data+stamps.toString()+sk+address
 const isValidProof = (proof: Proof): boolean => {
@@ -27,9 +38,30 @@ const isValidProof = (proof: Proof): boolean => {
     const isValidSign = pkToKey(proof.address).verify(proof.toStringForSign(), Buffer.from(proof.sign))
     return isValidStamps && isValidSign
 }
+const compareTime = (proof1: Proof, proof2: Proof): number => {
+    const findStamp = (stamps: Stamp[], address: Address) => stamps.find((stamp, _, __) => stamp.address == address)
+
+    const results: number[] = proof1.stamps.map((stamp, _, __) => {
+        const address = stamp.address
+        const stamp1 = findStamp(proof1.stamps, address)
+        const stamp2 = findStamp(proof2.stamps, address)
+        if(!stamp1 || !stamp2) {
+            return 0
+        }
+        const diff = stamp1.count - stamp2.count
+        if(diff == 0) {
+            return 0
+        }else if(diff > 0) {
+            return 1
+        }else {
+            return -1
+        }
+    })
+    return results.reduce((sum, k) => sum + k, 0)
+}
 
 const newProofSet = (proofs: Proof[]|undefined): Set<Proof> => {
     return new Set(proofs)
 }
 
-export { Stamp, isValidStamp, stampToStringForSign, Proof, proofToStringForSign, isValidProof, newProofSet }
+export { Stamp, isValidStamp, calcVDFResult, stampToStringForSign, Proof, proofToStringForSign, isValidProof, newProofSet, compareTime }
